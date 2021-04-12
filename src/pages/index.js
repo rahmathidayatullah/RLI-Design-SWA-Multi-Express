a5999ad281b3e981702ce09b986456063385046b
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-04-13 00:24:46 +0700

    page success

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,9 @@
-import React from "react";
-import Sidebar from "../components/sidebar";
-import Content from "../components/content";
-import DaftarTransaksi from "../components/content/DaftarTransaksi";
-import Pengaturan from "../components/content/Pengaturan";
-import TambahTransaksi from "../components/content/TambahTransaksi";
-import { Route, Switch } from "react-router-dom";
+import React from 'react'
 
-export default function Index() {
+export default function PageSuccess() {
   return (
-    <div className="flex items-start">
-      <div className="w-1/4 flex justify-center h-screen pt-6">
-        <Sidebar />
-      </div>
-      <div className="w-3/4 h-screen overflow-scroll px-5 pt-6">
-        <Content />
-        <Switch>
-          <Route path="/tambah-transaksi" component={TambahTransaksi} />
-        </Switch>
-        <Switch>
-          <Route path="/daftar-transaksi" component={DaftarTransaksi} />
-        </Switch>
-        <Switch>
-          <Route path="/pengaturan" component={Pengaturan} />
-        </Switch>
-      </div>
+    <div>
+      <h1>hello</h1>
     </div>
-  );
+  )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import Setting from 'pages/Setting'
 import Sidebar from 'components/sidebar'
 import GuardRoute from 'utils/guardRoute'
 import GuardOnlyRoute from 'utils/guardOnlyRoute'
+import Success from 'pages'
 
 
 const TrasactionPage = () => {
@@ -17,6 +18,20 @@ const TrasactionPage = () => {
         </div>
         <div className="w-3/4 h-screen overflow-scroll px-5 pt-6">
           <Trasaction />
+        </div>
+      </div>
+    </GuardRoute>
+  )
+}
+const SuccessPage = () => {
+  return (
+    <GuardRoute>
+      <div className="flex items-start">
+        <div className="w-1/4 flex justify-center h-screen pt-6">
+          <Sidebar />
+        </div>
+        <div className="w-3/4 h-screen overflow-scroll px-5 pt-6">
+          <Success />
         </div>
       </div>
     </GuardRoute>
@@ -69,6 +84,11 @@ const router = [
     path: "/transaction/create",
     exact: true,
     component: CreateTrasactionPage,
+  },
+  {
+    path: "/success",
+    exact: true,
+    component: SuccessPage,
   },
   {
     path: "/transaction",
